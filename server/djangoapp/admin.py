1e323b1a352de358dd16f0f11e667f0f257f2a16
Prashant Choudhari <prashantchoudhari74544@gmail.com>
2025-10-19 22:08:39 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'car_make', 'type', 'year')  # columns shown in admin list view
+
 # Registering models with their respective admins
 admin.site.register(CarMake)
-admin.site.register(CarModel)
+admin.site.register(CarModel, CarModelAdmin)
 
 
 # Register your models here.
