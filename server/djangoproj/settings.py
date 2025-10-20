422eb10bb59aa536d96f4b575fb432abba0f327a
Prashant Choudhari <prashantchoudhari74544@gmail.com>
2025-10-19 21:39:06 -0400

    Adding temporary changes to Github

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS=['localhost','https://prashantcho5-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS=['https://prashantcho5-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-'https://prashantcho5-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+'https://prashantcho5-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
+'https://prashantcho5-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
